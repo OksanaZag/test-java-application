304ba46c0f014b6cee1c3d6896a7293de09b9708
Oksana <Oksana@ThinkPadT60p>
2015-10-07 15:16:41 +0700

    Initial commit

---
+++ JavaTestApplication/junitapplication/src/main/java/MathFunction.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Oksana
+ */
+public class MathFunction {
+
+    public MathFunction(){
+    
+    }   
+    
+    public long sum(int operand1, int operand2){
+        return operand1+operand2;
+    }
+    
+    public long division(int operand1, int operand2){
+        return operand1/operand2;
+     }
+    
+    public long multiplication(int operand1, int operand2){
+        return operand1*operand2;
+    }
+    
+    public long subtraction(int operand1, int operand2){
+        return operand1-operand2;
+    }
+}
